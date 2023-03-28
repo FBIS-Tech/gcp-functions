a31ce622e46bc95c253d20eff7bc37898b4be6dc
jinad david <hashdavies@gmail.com>
2023-03-28 23:18:54 +0100

    added student dialling hisroty function

---
+++ daillingHistory/src/types/Student.ts
@@ -0,0 +1,11 @@
+export interface Student {
+    firstName: string,
+    lastName: string,
+    msisdn: number,
+    email: string
+    gender: string,
+    state: string,
+    totalSubs: number,
+    totalActiveSubs: number,
+    dateJoined: string
+}
